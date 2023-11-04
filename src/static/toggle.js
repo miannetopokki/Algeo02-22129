e4a6e6b30bd7108f6eef923da816f0229a857222
hugosabam <hugo.sasianturi@gmail.com>
2023-11-05 00:26:52 +0700

    update main + use html + css

---
+++ src/static/toggle.js
@@ -0,0 +1,7 @@
+// JavaScript code for the toggle button
+document.addEventListener('DOMContentLoaded', function() {
+    const toggle = document.getElementById('toggle');
+    toggle.addEventListener('change', function() {
+      // Lakukan sesuatu saat tombol toggle diubah, jika diperlukan
+    });
+  });
