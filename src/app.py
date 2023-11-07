d6cdaa46d2902b551f2691db718423ff26f700a9
hugosabam <hugo.sasianturi@gmail.com>
2023-11-07 21:55:11 +0700

    feat: delete img + bug fix

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_folder='static')
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
 app.config['DATASETS_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 'datasets')
 app.config['FILTER_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static')
-app.secret_key = 'keyjangandigantiplisdarihugo'  #buat save gambar kalo ganti page
+app.secret_key = 'keyjangandigantiplisdarihugo'  #buat save session kalo ganti page
 
 # Konfigurasi lokasi direktori 'uploads'
 
@@ -29,16 +29,49 @@ def about():
 
 @app.route('/use', methods=['GET', 'POST'])
 def use():
+
+    #Fungsi Hapus Gambar
+    if request.method == 'POST':
+    # Mendapatkan path gambar yang akan dihapus dari form
+        image_to_delete = request.form.get('image_path')
+
+        # Periksa apakah tombol delete ditekan
+        if 'delete_image' in request.form:
+            # Path lengkap ke file gambar yang akan dihapus
+            full_path = os.path.join(app.root_path, image_to_delete)
+
+            # Memastikan path file berada dalam folder 'datasets' dan merupakan file gambar
+            if os.path.exists(full_path) and full_path.startswith(os.path.join(app.root_path, 'static', 'datasets')) and \
+            (full_path.lower().endswith('.jpg') or full_path.lower().endswith('.png')):
+                try:
+                    # Hapus file gambar
+                    os.remove(full_path)
+                    print(f"File gambar {image_to_delete} berhasil dihapus.")
+                    session['image_url'] = None
+                    session['namafile'] = None
+                    session['messageunggah'] = "Berhasil Hapus foto!"
+                except Exception as e:
+                    print(f"Gagal menghapus file gambar: {str(e)}")
+
+
+
+
+
+
+
+    #DEKLARASI AWAL==============
     # Menampilkan maksimum 6 gambar per halaman
     page_size = 6
     page = int(request.args.get('page', 1))  # Halaman saat ini, defaultnya adalah halaman 1
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     # Path ke direktori datasets di dalam folder static
+
     dataset_dir = app.config['DATASETS_FOLDER']
     # Mengambil daftar file gambar dalam direktori datasets
     image_list = [os.path.join('static', 'datasets', filename) for filename in os.listdir(dataset_dir) if filename.endswith(('.jpg', '.png', '.jpeg'))]
     # Mengambil daftar gambar untuk halaman saat ini
+
     current_images = image_list[start_index:end_index]
     num_pages = math.ceil(len(image_list) / page_size)
     image_list = []
@@ -46,6 +79,27 @@ def use():
     image_url = None  # Tetapkan dengan nilai default
     namafile = None
     action = request.form.get('action')
+
+
+
+
+
+
+    if action == "Reset":
+        folder_path = os.path.join(os.path.dirname(__file__), 'static', 'filter')
+        try :
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path,filename)
+                if os.path.isfile(file_path) and (filename.lower().endswith('.jpg') or filename.lower().endswith('.png')):
+                    os.remove(file_path)
+            print("Semua file gambar (.jpg dan .png) dalam folder berhasil dihapus.")
+        except Exception as e:
+            print(f"Terjadi kesalahan: {str(e)}")
+
+
+
+
+
     if action == "Unggah":
         if request.method == 'POST':
             uploaded_file = request.files['file']
@@ -53,10 +107,10 @@ def use():
                 # Periksa ekstensi file yang diunggah
                 allowed_extensions = {'jpg', 'jpeg', 'png'}
                 if '.' in uploaded_file.filename and uploaded_file.filename.rsplit('.', 1)[1].lower() in allowed_extensions:
-                    file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+                    file_path = os.path.join(app.config['DATASETS_FOLDER'], uploaded_file.filename)
                     uploaded_file.save(file_path)
                     messageunggah = "Berhasil diunggah!"
-                    image_url = "static/uploads/" + uploaded_file.filename
+                    image_url = "static/datasets/" + uploaded_file.filename
                     namafile = uploaded_file.filename
 
                     # Simpan image_url dan namafile di sesi
@@ -66,53 +120,55 @@ def use():
                     messageunggah = "File yang diunggah tidak valid. Harap unggah file dengan ekstensi .jpg, .jpeg, atau .png."
             else:
                 messageunggah = "Gagal mengunggah file."
+        session['messageunggah'] = "Berhasil upload file!"
         messageunggah = session.get('messageunggah')
         image_url = session.get('image_url')
         namafile = session.get('namafile')
         return render_template('use.html', unggah=messageunggah, image_url=image_url, namafile=namafile, image_list=current_images, num_pages=num_pages, current_page=page)
             
+
+
+
     # Menghitung jumlah halaman total
     if action == 'Search':
         database_images = []
         image_url = session.get('image_url')
-        query_image = cv2.imread(image_url)
-        for img in glob.glob("./static/datasets/*.jpg"):
-            n = cv2.imread(img)
-            database_images.append(n)
-        start = time.time()
-        result = color_based_image_retrieval(query_image, database_images)
-        end =time.time()
-        duration = end-start
-        # # display matches
-        # cv2.imshow("Query image", query_image)
-        # for i, (image, similarity) in enumerate(result):
-        #     cv2.imshow(f"Match {i + 1} - Similarity: {similarity:.2f}%", image)
-        page_size = 6
-        page = int(request.args.get('page', 1))  # Halaman saat ini, defaultnya adalah halaman 1
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        # Path ke direktori datasets di dalam folder static
-        # Mengambil daftar file gambar dalam direktori datasets
-        image_list = result
-        # Mengambil daftar gambar untuk halaman saat ini
-        messageunggah = session.get('messageunggah')
-        namafile = session.get('namafile')
-        output_folder = os.path.join(app.config['FILTER_FOLDER'], 'filter')
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-        filteredImages = []
-        # Simpan gambar-gambar hasil filter
-        for i, (image, similarity) in enumerate(image_list):
-            output_filename = f'result{i + 1}.jpg'
-            output_path = os.path.join(output_folder, output_filename)
-            cv2.imwrite(output_path, image)
-            filteredImages.append((output_path,similarity))
+        if (image_url != None):
+            query_image = cv2.imread(image_url)
+            for img in glob.glob("./static/datasets/*.jpg"):
+                n = cv2.imread(img)
+                database_images.append(n)
+            start = time.time()
+            result = color_based_image_retrieval(query_image, database_images)
+            end =time.time()
+            duration = end-start
+            page_size = 20
+            page = int(request.args.get('page', 1))  # Halaman saat ini, defaultnya adalah halaman 1
+            start_index = (page - 1) * page_size
+            end_index = start_index + page_size
+            image_list = result
+            messageunggah = session.get('messageunggah')
+            namafile = session.get('namafile')
+            output_folder = os.path.join(app.config['FILTER_FOLDER'], 'filter')
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+            filteredImages = []
+            # Simpan gambar-gambar hasil filter
+            for i, (image, similarity) in enumerate(image_list):
+                output_filename = f'result{i + 1}.jpg'
+                output_path = os.path.join(output_folder, output_filename)
+                cv2.imwrite(output_path, image)
+                filteredImages.append((output_path,similarity))
+            # Sekarang, baca daftar gambar hasil filter dari direktori 'filter'
+            image_list = [os.path.join('static', 'filter', os.path.basename(image_path)) for image_path, similarity in filteredImages]
+            current_images = image_list[start_index:end_index]
+            session['messageunggah'] = "Search Berhasil!"
+            messageunggah = session.get('messageunggah')
+            return render_template('use.html', unggah=messageunggah, image_url=image_url, namafile=namafile, result =filteredImages ,image_list=current_images, num_pages=num_pages, current_page=page, durasi=duration)
+        else:
+            session['messageunggah'] = "Error! Anda mau search apa wkwkwk"
+            messageunggah = session.get('messageunggah')
 
-
-        # Sekarang, baca daftar gambar hasil filter dari direktori 'filter'
-        image_list = [os.path.join('static', 'filter', os.path.basename(image_path)) for image_path, similarity in filteredImages]
-        current_images = image_list[start_index:end_index]
-        return render_template('use.html', unggah=messageunggah, image_url=image_url, namafile=namafile, result =filteredImages ,image_list=current_images, num_pages=num_pages, current_page=page, durasi=duration)
 
 
 
